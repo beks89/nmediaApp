b579249d72ac1c2825e9a179532ed6d093f20893
Kris <beks89@yandex.ru>
2025-09-27 15:54:45 +0300

    Add recycleView

--- app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
@@ -6,50 +6,31 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import ru.netology.nmedia.R
+import ru.netology.nmedia.adapter.PostsAdapter
 import ru.netology.nmedia.databinding.ActivityMainBinding
-import ru.netology.nmedia.dto.countFormat
 import ru.netology.nmedia.viewModel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
-
-    private val viewModel by viewModels<PostViewModel>()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.displayCutout() or WindowInsetsCompat.Type.systemBars())
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        viewModel.get().observe(this) { post ->
-        with(binding) {
-            author.text = post.author
-            content.text = post.content
-            published.text = post.published
-            likesCount.text = countFormat(post.likesCount)
-            shareCount.text = countFormat(post.sharesCount)
-            visibleCount.text = countFormat(post.viewsCount)
-
-            likes.setImageResource(
-                if (post.likedByMe) R.drawable.ic_like_red
-                else R.drawable.ic_like
-                )
-            }
+        val viewModel: PostViewModel by viewModels()
+        val adapter = PostsAdapter({
+            viewModel.likeById(it.id)
+        },{
+            viewModel.shareById(it.id)
+        })
+        binding.list.adapter = adapter
+        viewModel.data.observe(this) { posts ->
+            adapter.list = posts
         }
-
-        binding.likes.setOnClickListener {
-            viewModel.like()
-        }
-
-        binding.share.setOnClickListener {
-            viewModel.share()
-        }
-
     }
 }
--- app/src/main/java/ru/netology/nmedia/viewModel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewModel/PostViewModel.kt
@@ -1,21 +1,12 @@
 package ru.netology.nmedia.viewModel
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.repository.PostRepository
 import ru.netology.nmedia.repository.PostRepositoryInMemoryImpl
 
 class PostViewModel : ViewModel() {
     private val repository: PostRepository = PostRepositoryInMemoryImpl()
-
-    fun get(): LiveData<Post> = repository.get()
-
-    fun like() {
-        repository.like()
-    }
-
-    fun share() {
-        repository.share()
-    }
+    val data = repository.getAll()
+    fun likeById(id: Long) = repository.likeById(id)
+    fun shareById(id: Long) = repository.shareById(id)
 }
