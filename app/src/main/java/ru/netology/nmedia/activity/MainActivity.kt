285a0cdaf24227ac56e8aeb4414eb19966956653
Kris <beks89@yandex.ru>
2025-09-27 16:59:53 +0300

    Add ListAdapter

--- app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
@@ -30,7 +30,7 @@ class MainActivity : AppCompatActivity() {
         })
         binding.list.adapter = adapter
         viewModel.data.observe(this) { posts ->
-            adapter.list = posts
+            adapter.submitList(posts)
         }
     }
 }
--- app/src/main/java/ru/netology/nmedia/adapter/PostsAdapter.kt
+++ app/src/main/java/ru/netology/nmedia/adapter/PostsAdapter.kt
@@ -2,35 +2,32 @@ package ru.netology.nmedia.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import ru.netology.nmedia.R
 import ru.netology.nmedia.databinding.CardPostBinding
 import ru.netology.nmedia.dto.Post
+import ru.netology.nmedia.dto.countFormat
 
 
 typealias OnLikeListener = (post: Post) -> Unit
 typealias OnShareListener = (post: Post) -> Unit
 
-class PostsAdapter(private val onLikeListener: OnLikeListener, private val onShareListener: OnShareListener) : RecyclerView.Adapter<PostViewHolder>() {
-    var list = emptyList<Post>()
-        set(value) {
-            field = value
-            notifyDataSetChanged()
-        }
-
+class PostsAdapter(
+    private val onLikeListener: OnLikeListener,
+    private val onShareListener: OnShareListener
+) : ListAdapter<Post, PostViewHolder>(PostDiffCallback) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
         val binding = CardPostBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return PostViewHolder(binding, onLikeListener, onShareListener)
     }
 
     override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
-        val post = list[position]
+        val post = getItem(position)
         holder.bind(post)
     }
-
-    override fun getItemCount(): Int = list.size
 }
-
 class PostViewHolder(
     private val binding: CardPostBinding,
     private val onLikeListener: OnLikeListener,
@@ -42,8 +39,8 @@ class PostViewHolder(
             author.text = post.author
             published.text = post.published
             content.text = post.content
-            likesCount.text = post.likesCount.toString()
-            sharesCount.text = post.sharesCount.toString()
+            likesCount.text = countFormat(post.likesCount)
+            sharesCount.text = countFormat(post.sharesCount)
 
             likes.setImageResource(
                 if (post.likedByMe) R.drawable.ic_like_red else R.drawable.ic_like)
@@ -56,4 +53,16 @@ class PostViewHolder(
             }
         }
     }
+}
+
+object PostDiffCallback : DiffUtil.ItemCallback<Post>() {
+
+    override fun areItemsTheSame(oldItem: Post, newItem: Post): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Post, newItem: Post): Boolean {
+        return oldItem == newItem
+    }
+
 }
