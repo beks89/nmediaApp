551aa1dcbb04ded121b045860db3fe10e6f765e6
Kris <beks89@yandex.ru>
2026-02-14 18:20:43 +0300

    Lesson code

--- app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
@@ -2,6 +2,8 @@ package ru.netology.nmedia.repository
 
 import kotlinx.coroutines.flow.Flow
 import ru.netology.nmedia.dto.Post
+import ru.netology.nmedia.dto.Media
+import ru.netology.nmedia.dto.MediaUpload
 
 interface PostRepository {
     val data: Flow<List<Post>>
@@ -13,5 +15,7 @@ interface PostRepository {
     suspend fun removeByIdAsync(id: Long)
     suspend fun getAllAsync()
     suspend fun updateIsRead()
+    suspend fun saveWithAttachment(post: Post, upload: MediaUpload)
+    suspend fun upload(upload: MediaUpload): Media
 
 }
