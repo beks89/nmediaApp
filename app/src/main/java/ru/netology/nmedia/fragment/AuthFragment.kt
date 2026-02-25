df2ea90a1fd7303e1dcdfa87494156d4304a087d
Kris <beks89@yandex.ru>
2026-02-25 15:10:59 +0300

    fix Firebase module

--- app/src/main/java/ru/netology/nmedia/fragment/AuthFragment.kt
+++ app/src/main/java/ru/netology/nmedia/fragment/AuthFragment.kt
@@ -5,7 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.viewModels
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
@@ -17,7 +17,8 @@ import ru.netology.nmedia.viewModel.AuthViewModel
 @AndroidEntryPoint
 class AuthFragment : Fragment() {
 
-    private val viewModel: AuthViewModel by viewModels(ownerProducer = ::requireParentFragment)
+    //private val viewModel: AuthViewModel by viewModels(ownerProducer = ::requireParentFragment)
+    private val viewModel: AuthViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
